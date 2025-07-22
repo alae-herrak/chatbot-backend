9f34bb1c2bf8d0d61d3f9dcb444b1d4a8f82d455
PC <kawtar.benali@etu.uae.ac.ma>
2025-07-22 15:18:17 +0200

    v1

--- preload_utils.py
+++ preload_utils.py
@@ -2,6 +2,9 @@ from models import db, Response, Category
 from text_api import preload_language_data, cache
 
 def preload_all_languages():
+    if "text_embeddings" not in cache:
+        cache["text_embeddings"] = {}
+
     for lang in ['fr', 'en', 'ar']:
-        if lang not in cache["tfidf_matrix"]:
+        if lang not in cache["text_embeddings"]:
             preload_language_data(lang)
